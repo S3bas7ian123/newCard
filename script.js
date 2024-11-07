51501c0092bdd19494c1648ac7c4addb2686318b
Sebastian Levi <sebastianLevi@gmail.com>
2024-11-07 18:15:24 +0100

    3) Third commit: new changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const main = document.querySelector('main')
 const article = document.querySelector('article');
-const section = document.querySelector('section');
+// const section = document.querySelector('section');
+//const div = document.querySelector('.containerText');
+const titleTwo = document.querySelector('h2');
+const paragraph = document.querySelector('p');
 
 main.classList.add('mainSet');
 article.classList.add('articleSet');
+titleTwo.textContent = 'Welcome to my House';
+paragraph.textContent = 'This very fancy house belonged to Mr. Jhon the Wise, a programmer who was terrible at JavaScript. He became obsessed with the programming language, and as soon as he bought this house, he stayed inside to study it all the time. P.S: he never came out sane.';
