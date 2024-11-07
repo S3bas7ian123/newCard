a2ac1416c0295e1f80216b88f1f832efbeb317c9
Sebastian Levi <sebastianLevi@gmail.com>
2024-11-07 17:45:13 +0100

    2) Second commit: settings for main and article

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.querySelector('main')
+const article = document.querySelector('article');
+const section = document.querySelector('section');
+
+main.classList.add('mainSet');
+article.classList.add('articleSet');
